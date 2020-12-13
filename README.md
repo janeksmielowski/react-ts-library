eb892cc43be1267cf5a58996b0386cbba3c13873
jansmi <janek.smielowski@gmail.com>
2020-12-13 18:20:35 +0100

    Adjustments for library exposal

--- README.md
+++ README.md
@@ -1,25 +1,16 @@
-# React Typescript Template
+# React Typescript Library Template
 
-This project contains a basic [React](https://github.com/facebook/react) application template configuration with the following extensions:
+This project contains a basic [React](https://github.com/facebook/react) library template configuration with the following extensions:
 - [TypeScript](https://github.com/microsoft/TypeScript)
 - [Webpack](https://github.com/webpack/webpack)
 - [Babel](https://github.com/babel/babel)
 - [ESLint](https://github.com/eslint/eslint)
 - [Prettier](https://github.com/prettier/prettier)
 - [Jest](https://github.com/facebook/jest)
-- [Styled Components](https://github.com/styled-components/styled-components)
 
 ## Available Scripts
 
 In the project directory, you can run:
-
-### `yarn start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
 
 ### `yarn test`
 
@@ -28,10 +19,8 @@ Launches the test runner in the interactive watch mode.
 ### `yarn build`
 
 Builds the app for production to the `dist` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+Your library is ready to be deployed!
 
 ## Learn More
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,28 @@
 const path = require('path');
 
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { ProgressPlugin } = require('webpack');
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = function(env, argv) {
-    const isProduction = argv.mode === 'production';
-    
     return {
         mode: argv.mode,
         entry: path.resolve(__dirname, 'src/index.tsx'),
         output: {
-            chunkFilename: isProduction ? '[name].[chunkhash:4].chunk.js' : '[name].js',
-            filename: isProduction ? '[name].[contenthash:4].js' : '[name].js',
+            filename: 'index.js',
+            library: '$',
+            libraryTarget: 'umd',
             path: path.resolve(__dirname, 'dist'),
-            publicPath: '/'
         },
-        target: 'web',
-        devtool: !isProduction && 'source-map',
+        devtool: 'source-map',
         module: {
             rules: [
                 {
                     test: /\.(js|ts)x?$/,
                     exclude: /node_modules/,
-                    loader: 'babel-loader',
-                    options: {
-                        cacheDirectory: true
-                    }
-                },
-                {
-                    exclude: [/\.(js|ts)x?$/, /\.html$/, /\.json$/],
-                    loader: 'file-loader',
-                    options: {
-                        name: isProduction ? '[name].[contenthash:4].[ext]' : '[name].[ext]'
-                    }
+                    use: ['babel-loader', 'ts-loader']
                 }
             ]
-        },
-        optimization: {
-            chunkIds: 'natural',
-            minimize: isProduction,
-            minimizer: [new TerserPlugin()],
-            splitChunks: {
-                chunks: 'all',
-            },
-            runtimeChunk: true
         },
         resolve: {
             alias: {
@@ -61,36 +36,7 @@ module.exports = function(env, argv) {
             new ESLintPlugin({
                 extensions: ['.js', '.jsx', '.ts', '.tsx']
             }),
-            new CleanWebpackPlugin(),
-            new CopyWebpackPlugin({
-                patterns: [
-                    {
-                        from: './public/assets',
-                        to: 'assets'
-                    },
-                    {
-                        from: './public/manifest.json',
-                        to: 'manifest.json'
-                    },
-                    {
-                        from: './public/robots.txt',
-                        to: 'robots.txt'
-                    }
-                ]
-            }),
-            new HtmlWebpackPlugin({
-                template: './public/index.html'
-            })
-        ],
-        devServer: {
-            compress: true,
-            contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true,
-            host: 'localhost',
-            hot: true,
-            open: true,
-            port: 3000,
-            progress: true
-        }
+            new CleanWebpackPlugin()
+        ]
     };
 }
